dd41b6ac022ecabc41979ba9d9f41efb6a1bcdcc
TomaBoykova <tomassb@mail.ru>
2022-06-02 21:47:29 +0700

    Finished mobile adaptation

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ document.querySelector('.menu-icon-wrapper').onclick = function(){
    // document.querySelector('.nav__list-holder').style.display = "block";
    document.querySelector('.nav__list-holder').classList.toggle('.nav__list-holder--visible');
 };
+
+
+const scrollBth = document.querySelector('.scrollTop');
+
+window.onscroll = () => {
+   if (window.scrollY > 600) {
+      scrollBth.classList.remove('scrollTop_hide');
+   } else if (window.scrollY < 900) {
+      scrollBth.classList.add('scrollTop_hide');
+   }
+};
+
+scrollBth.onclick = () => {
+   window.scrollTo(0, 0);
+
+};
